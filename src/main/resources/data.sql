761aee220538506573bbd340344193630b598f31
daniil iljin <d.iljn.nkl@gmail.com>
2024-03-18 15:41:09 +0200

    added new data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,20 @@
-INSERT INTO location (id, name, address, description)
-VALUES (1, 'Nilat', 'Akadeemia tee', 'Closest plase');
+INSERT INTO location (name, address, description)
+VALUES ('Nilat', 'Akadeemia tee', 'Closest plase');
 
-INSERT INTO category (id, name, description, location_id)
-VALUES (1, 'Package', 'Clean package', 1);
+INSERT INTO category (name, description, location_id)
+VALUES ('Package', 'Clean package', 1);
 
-INSERT INTO sub_category (id, name, description, category_id)
-VALUES (1, 'Bottles', 'Clean bottles', 1);
+INSERT INTO category (name, description)
+VALUES ('Electronics', 'All sorts of electronics');
 
-INSERT INTO item ( name, description, sub_category_id)
+INSERT INTO category (name, description)
+VALUES ('BIO waste', 'All organic waste');
+
+INSERT INTO sub_category (name, description, category_id)
+VALUES ('Bottles', 'Clean bottles', 1);
+
+INSERT INTO sub_category (name, description, category_id)
+VALUES ('Plastic', 'Plastic packaging', 1);
+
+INSERT INTO item (name, description, sub_category_id)
 VALUES ('Glass bottle', 'Package must be clean', 1);
